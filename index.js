0b023d5b4a9cf535888956b9078f8a6db44f71c6
Shivam Bansal <shivam2032.cse18@chitkara.edu.in>
2021-05-13 23:10:08 +0530

    adding a reducer

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ function buycake(){
     info: 'First redux action'
     }
 }
+
+//(previousState,action)
+
+const initialState={
+    numofcakes: 10
+}
+
+const reducer=(state=initialState,action)=>{
+    switch(action.type){
+        case Buy_Cake: return{
+            ...state,
+            numofcakes: state.numofcakes -1
+        }
+        default: return state
+    }
+}
